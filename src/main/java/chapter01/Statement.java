a3c381e4378b205a6a7c9db679ba485fa6a0cc24
dmyun23 <dmyun23@gmail.com>
2023-09-17 23:06:02 +0900

    공연에 대한 요금 계산하는 case문을 amountFor 함수로 추출하기

--- src/main/java/chapter01/Statement.java
+++ src/main/java/chapter01/Statement.java
@@ -18,23 +18,9 @@ public class Statement {
             Play play = plays.get(perf);
             int thisAmount = 0;
 
-            switch(play.getType()){
-                case TRAGEDY : // 비극
-                    thisAmount = 40000;
-                    if(perf.getAudience()>30){
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case CODMEDY : //희극
-                    thisAmount = 30000;
-                    if(perf.getAudience() > 20){
-                        thisAmount += 10000 + 500 * ( perf.getAudience() -20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new Error("알 수 없는 장르: " + play.getType());
-            }
+            // 1. 함수 추출하기 기법. 각종 case문을 amountFor 함수로 대체
+            thisAmount = amountFor(perf, play);
+
             // 포인트를 적립한다.
             volumeCredits += Math.max(perf.getAudience() -30, 0);
             //희극 관객 5명마다 추가 포인트를 제공한다.
@@ -49,5 +35,29 @@ public class Statement {
         result += String.format("적립 포인트: %d점", volumeCredits);
         System.out.println("결과 : "+ result);
         return result;
+    }
+
+    public int amountFor(Performance perf, Play play){
+        int thisAmount = 0;
+
+        switch(play.getType()){
+            case TRAGEDY : // 비극
+                thisAmount = 40000;
+                if(perf.getAudience()>30){
+                    thisAmount += 1000 * (perf.getAudience() - 30);
+                }
+                break;
+            case CODMEDY : //희극
+                thisAmount = 30000;
+                if(perf.getAudience() > 20){
+                    thisAmount += 10000 + 500 * ( perf.getAudience() -20);
+                }
+                thisAmount += 300 * perf.getAudience();
+                break;
+            default:
+                throw new Error("알 수 없는 장르: " + play.getType());
+        }
+
+        return thisAmount;
     }
 }
