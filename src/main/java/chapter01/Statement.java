4bdef44819711173fa341946552eed70c3bb2909
dmyun23 <dmyun23@gmail.com>
2023-09-23 18:32:23 +0900

    임수변수를 질의 함수로 바꾸기

--- src/main/java/chapter01/Statement.java
+++ src/main/java/chapter01/Statement.java
@@ -14,21 +14,20 @@ public class Statement {
         int totalAmount = 0;
         int volumeCredits = 0;
         String result = String.format("청구내역 (고객명: %s)\n", invoice.getCustomer());
-        for(Performance perf:invoice.getPerformances()){
-            Play play = plays.get(perf);
+        for(Performance performance:invoice.getPerformances()){
             int thisAmount = 0;
 
             // 1. 함수 추출하기 기법. 각종 case문을 amountFor 함수로 대체
-            thisAmount = amountFor(perf, play);
+            thisAmount = amountFor(performance, plays);
 
             // 포인트를 적립한다.
-            volumeCredits += Math.max(perf.getAudience() -30, 0);
+            volumeCredits += Math.max(performance.getAudience() -30, 0);
             //희극 관객 5명마다 추가 포인트를 제공한다.
-            if(PlayType.CODMEDY == play.getType()){
-                volumeCredits += Math.floor(perf.getAudience() /5 );
+            if( playFor(plays,performance).getType().equals(PlayType.CODMEDY)){
+                volumeCredits += Math.floor(performance.getAudience() /5 );
             }
             //청구 내역을 출력한다.
-            result += String.format("%s: $%d %d석\n", play.getName(),Math.round(thisAmount/100),perf.getAudience() );
+            result += String.format("%s: $%d %d석\n", playFor(plays,performance).getName(),Math.round(thisAmount/100),performance.getAudience() );
             totalAmount += thisAmount;
         }
         result += String.format("총액: $%d\n", Math.round(totalAmount/100));
@@ -37,27 +36,28 @@ public class Statement {
         return result;
     }
 
-    public int amountFor(Performance aPerformance, Play play){
+    public int amountFor(Performance performance, Plays plays){
         int result = 0;
-
-        switch(play.getType()){
+        switch(playFor(plays,performance).getType()){
             case TRAGEDY : // 비극
                 result = 40000;
-                if(aPerformance.getAudience()>30){
-                    result += 1000 * (aPerformance.getAudience() - 30);
+                if(performance.getAudience()>30){
+                    result += 1000 * (performance.getAudience() - 30);
                 }
                 break;
             case CODMEDY : //희극
                 result = 30000;
-                if(aPerformance.getAudience() > 20){
-                    result += 10000 + 500 * ( aPerformance.getAudience() -20);
+                if(performance.getAudience() > 20){
+                    result += 10000 + 500 * ( performance.getAudience() -20);
                 }
-                result += 300 * aPerformance.getAudience();
+                result += 300 * performance.getAudience();
                 break;
             default:
-                throw new Error("알 수 없는 장르: " + play.getType());
+                throw new Error("알 수 없는 장르: " + playFor(plays,performance).getType());
         }
-
         return result;
+    }
+    public Play playFor(Plays plays, Performance Performance) {
+        return plays.get(Performance);
     }
 }
