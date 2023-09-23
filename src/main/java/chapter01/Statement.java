f97fdadba4dc4fb46fb99a4723891bd77f4d827c
dmyun23 <dmyun23@gmail.com>
2023-09-23 18:48:54 +0900

    변수 인라인하기

--- src/main/java/chapter01/Statement.java
+++ src/main/java/chapter01/Statement.java
@@ -13,30 +13,21 @@ public class Statement {
 
         int totalAmount = 0;
         int volumeCredits = 0;
-        String result = String.format("청구내역 (고객명: %s)\n", invoice.getCustomer());
+        StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
         for(Performance performance:invoice.getPerformances()){
-            int thisAmount = 0;
-
-            // 1. 함수 추출하기 기법. 각종 case문을 amountFor 함수로 대체
-            thisAmount = amountFor(performance, plays);
-
-            // 포인트를 적립한다.
-            volumeCredits += Math.max(performance.getAudience() -30, 0);
-            //희극 관객 5명마다 추가 포인트를 제공한다.
-            if( playFor(plays,performance).getType().equals(PlayType.CODMEDY)){
-                volumeCredits += Math.floor(performance.getAudience() /5 );
-            }
+            volumeCredits += volumeCreditFor(plays, performance);
             //청구 내역을 출력한다.
-            result += String.format("%s: $%d %d석\n", playFor(plays,performance).getName(),Math.round(thisAmount/100),performance.getAudience() );
-            totalAmount += thisAmount;
+            result.append(String.format("%s: $%d %d석\n",
+                            playFor(plays,performance).getName(),amountFor(performance, plays)/100,performance.getAudience()
+                        ));
+            totalAmount += amountFor(performance, plays);;
         }
-        result += String.format("총액: $%d\n", Math.round(totalAmount/100));
-        result += String.format("적립 포인트: %d점", volumeCredits);
-        System.out.println("결과 : "+ result);
-        return result;
+        result.append(String.format("총액: $%d\n", totalAmount/100));
+        result.append(String.format("적립 포인트: %d점", volumeCredits));
+        return result.toString();
     }
 
-    public int amountFor(Performance performance, Plays plays){
+    private int amountFor(Performance performance, Plays plays){
         int result = 0;
         switch(playFor(plays,performance).getType()){
             case TRAGEDY : // 비극
@@ -57,7 +48,17 @@ public class Statement {
         }
         return result;
     }
-    public Play playFor(Plays plays, Performance Performance) {
+    private Play playFor(Plays plays, Performance Performance) {
         return plays.get(Performance);
+    }
+    private int volumeCreditFor(Plays plays, Performance performance){
+        int resulte = 0;
+        // 포인트를 적립한다.
+        resulte += Math.max(performance.getAudience() -30, 0);
+        //희극 관객 5명마다 추가 포인트를 제공한다.
+        if( playFor(plays,performance).getType().equals(PlayType.CODMEDY)){
+            resulte += Math.floor(performance.getAudience() /5 );
+        }
+        return resulte;
     }
 }
