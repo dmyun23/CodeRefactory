5c6b45a17cab02eb0071eb719dbff6527f2de160
dmyun23 <dmyun23@gmail.com>
2023-09-30 21:16:46 +0900

    함수 추출하기  - 리팩토링 후 소스 commit

--- src/main/java/chapter06/extractMethod01/After.java
+++ src/main/java/chapter06/extractMethod01/After.java
@@ -5,24 +5,37 @@ import java.time.LocalDateTime;
 public class After {
     public String pringOwing(Invoice invoice){
         StringBuilder sb = new StringBuilder();
-        int outstanding = 0;
 
-        sb.append("*****************");
-        sb.append("**** 고객 채무 ****");
-        sb.append("*****************");
+        printBanner(sb);
+        int outstanding = calculateOutStanding(invoice);
+        recordDueDate(invoice);
+        printDetail(sb,invoice,outstanding);
 
-        // 미해결 채무(outstanding)을 계산한다.
-        for(Order o : invoice.getOrders()){
-            outstanding += o.amount;
-        }
-
-        // 마감일(dueDate)을 기록한다.
-        invoice.dueDate = Clock.today();
-
-        // 세부 사항을 출력한다.
-        sb.append(String.format("고객명 : %s",invoice.customer));
-        sb.append(String.format("채무액 : %d",outstanding));
-        sb.append(String.format("마감일 : %s",invoice.dueDate));
         return sb.toString();
+    }
+
+    private void printBanner(StringBuilder result) {
+        result.append("*****************");
+        result.append("**** 고객 채무 ****");
+        result.append("*****************");
+    }
+    // 미해결 채무(outstanding)을 계산한다.
+    private int calculateOutStanding(Invoice invoice){
+        int result = 0;
+
+        for(Order o : invoice.getOrders()){
+            result += o.amount;
+        }
+        return result;
+    }
+    // 마감일(dueDate)을 기록한다.
+    private void recordDueDate(Invoice invoice){
+        invoice.dueDate = Clock.today();
+    }
+    // 세부 사항을 출력한다.
+    private void printDetail(StringBuilder result,Invoice invoice, int outstanding){
+        result.append(String.format("고객명 : %s",invoice.customer));
+        result.append(String.format("채무액 : %d",outstanding));
+        result.append(String.format("마감일 : %s",invoice.dueDate));
     }
 }
