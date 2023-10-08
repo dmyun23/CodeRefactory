02937f161ba46201c008ac115e90a7e5f89f2a04
dmyun23 <dmyun23@gmail.com>
2023-10-09 00:02:09 +0900

    chap07 중개자 제거하기  - example class commit

---
+++ src/main/java/chapter07/removeMiddleMan/Person.java
@@ -0,0 +1,18 @@
+package chapter07.removeMiddleMan;
+
+public class Person {
+    protected String name;
+    protected Department department;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
